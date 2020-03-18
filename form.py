1d1b5eb07dd3b7b920127ecc3ccf510110362abc
osuperdu <34589867+dutitello@users.noreply.github.com>
2020-03-18 09:03:36 -0300

    stupid identation corretion

--- form.py
+++ form.py
@@ -1381,7 +1381,7 @@ class FORM(object):
 			# conditional lastcycle below too. 
 			#### Sometimes it didn't converge because if this crap
 			relErrorPoint = max(abs((curVecRedPts-newVecRedPts)/newVecRedPts))
-		 	absErrorBeta = abs(curBeta-newBeta)
+			absErrorBeta = abs(curBeta-newBeta)
 			self._PrintR('Maximum relative error on design point = %1.4f.' % relErrorPoint)
 			self._PrintR('Absolute error betwen current and next beta = %1.4f.' % absErrorBeta)
 			#### if abs(valG) < self.controls['tolLS'] and (1-cosYgradY) < self.controls['tolRel']:
